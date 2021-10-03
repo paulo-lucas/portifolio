7d1d5b3be5b6425ec084a8b32791529759f3229a
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-10-03 13:21:32 -0300

    wip: creating contact component and contents file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Flex, Heading, IconButton, VStack, Spacer } from "@chakra-ui/react";
-import { FaInstagram, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { ColorModeSwitcher } from "components/ColorModeSwitcher";
+import { VStack } from "@chakra-ui/layout";
+import Contact from "components/Contact";
 import Header from "components/Header";
 import Profile from "components/Profile";
 import Social from "components/Social";
@@ -8,36 +7,7 @@ import Social from "components/Social";
 function App() {
   return (
     <VStack p={5}>
-      <Flex w="100%">
-        <Heading
-          ml="8"
-          size="md"
-          fontWeight="semibold"
-          color="cyan.400" >
-          plucas
-        </Heading>
-
-        <Spacer />
-
-        <a href="https://www.linkedin.com/in/plucas-cer">
-          <IconButton icon={<FaLinkedin />} isRound='true' />
-        </a>
-
-        <a href="https://www.instagram.com/paulo_lucasoc/">
-          <IconButton ml={2} icon={<FaInstagram />} isRound='true' />
-        </a>
-
-        <a href="https://twitter.com/plucasdev">
-          <IconButton ml={2} icon={<FaTwitter />} isRound='true' />
-        </a>
-
-        <a href="https://github.com/paulo-lucas">
-          <IconButton ml={2} icon={<FaGithub />} isRound='true' />
-        </a>
-
-        <ColorModeSwitcher ml={8} isRound={true} />
-      </Flex>
-
+      <Contact />
       <Header />
       <Profile />
       <Social />
---
+++ src/components/Contact.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+    Flex,
+    Heading,
+    IconButton,
+    Spacer
+} from "@chakra-ui/react";
+import {
+    FaInstagram,
+    FaGithub,
+    FaLinkedin,
+    FaTwitter
+} from "react-icons/fa";
+import { ColorModeSwitcher } from "components/ColorModeSwitcher";
+import { contact } from 'content.json'
+
+function Contact() {
+    const { heading, links } = contact;
+
+    return <Flex w="100%">
+        <Heading
+            ml="8"
+            size="md"
+            fontWeight="semibold"
+            color="cyan.400" >
+            {heading}
+        </Heading>
+
+        <Spacer />
+
+        <IconButton
+            icon={<FaLinkedin />}
+            isRound='true'
+            onClick={() => window.open(links.linkedin)} />
+
+        <IconButton
+            ml={2}
+            icon={<FaInstagram />}
+            isRound='true'
+            onClick={() => window.open(links.instagram)} />
+
+
+        <IconButton
+            ml={2}
+            icon={<FaTwitter />}
+            isRound='true'
+            onClick={() => window.open(links.twitter)} />
+
+
+        <IconButton
+            ml={2}
+            icon={<FaGithub />}
+            isRound='true'
+            onClick={() => window.open(links.github)} />
+
+
+        <ColorModeSwitcher ml={8} isRound={true} />
+    </Flex>
+}
+
+export default Contact;
