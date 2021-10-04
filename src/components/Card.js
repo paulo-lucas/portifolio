f79e8e6abd73f4c32733890e21a94811834f5c56
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-10-03 21:54:43 -0300

    portifolio section with fake data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+    Text,
+    Icon,
+    Flex,
+    useMediaQuery,
+    HStack,
+    Image,
+    Fade,
+    VStack
+} from '@chakra-ui/react';
+import { getTechIcon } from "assets/icons"
 
-function Card(props) {
+function Card({ projectName, description, tech, mainImage }) {
+    const [expandedContainer, setExpandedContainer] = useState(false);
+    const [expandedContent, setExpandedContent] = useState(false);
+    const [collapsedContent, setCollapsedContent] = useState(true);
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+    useEffect(() => {
+        handleInstantContentFade();
+        setTimeout(handleFutureContentFade, 400);
+    }, [expandedContainer])
+
+
+    function handleInstantContentFade() {
+        setCollapsedContent(false);
+        setExpandedContent(false);
+    }
+
+    function handleFutureContentFade() { 
+        setExpandedContent(expandedContainer);
+        setCollapsedContent(!expandedContainer);
+    }
+
     return <Flex
         rounded="xl"
+        boxShadow="lg"
         direction="column"
         mt={4}
         ml={isNotSmallerScreen ? 4 : 0}
-        bg="gray.100"
-        h="30vh"
+        h="45vh"
         w="30vh"
-        justify="flex-end"
-        _hover={{ bg: "green.400", }} >
+        overflow="hidden"
+        pos="relative"
+        cursor="pointer"
+        transition="all 0.2s ease-out"
+        _hover={{
+            "-webkit-transform": "translate(0, -10px)",
+            "-o-transform": "translate(0, -10px)",
+            "transform": "translate(0, -10px)"
+        }}
+        onClick={() => setExpandedContainer(!expandedContainer)} >
 
-        <Icon
-            as={DiWebplatform}
-            p="4"
-            w="24"
-            h="24"
-            color="black" />
+        <Image src={mainImage} />
 
-        <Text
-            color="black"
-            p="4"
-            fontSize="xl"
-            fontWeight="semibold" >
-            Web Apps
-        </Text>
-    </Flex>
+        <HStack
+            w="100%"
+            position="absolute"
+            bottom="0"
+            h={expandedContainer ? "100%" : "15vh"}
+            p={expandedContainer ? "8" : "3"}
+            align={expandedContainer ? "flex-start" : "center"}
+            bgColor="cyan.400"
+            transition="height 0.5s cubic-bezier(.8,-0.1,.3,1.3)" >
+
+            <Fade in={collapsedContent} unmountOnExit>
+                <HStack>
+                    <Icon
+                        mr="2"
+                        as={getTechIcon(tech)}
+                        w="20"
+                        h="20"
+                        color="white" />
+
+                    <Text
+                        color="white"
+                        fontSize="xl"
+                        fontWeight="semibold" >
+                        {projectName}
+                    </Text>
+                </HStack>
+            </Fade>
+
+            <Fade in={expandedContent} unmountOnExit>
+                <VStack
+                    alignItems="flex-start"
+                    justifyContent="flex-end" >
+                    <Text
+                        color="white"
+                        fontSize="xl"
+                        fontWeight="semibold" >
+                        {projectName}
+                    </Text>
+
+                    <Text
+                        color="white"
+                        fontSize="lg" >
+                        {description}
+                    </Text>
+
+                </VStack>
+            </Fade>
+        </HStack>
+    </Flex >
 }
+
+export default Card
--- src/components/Portifolio.js
+++ src/components/Portifolio.js
@@ -1,137 +1,59 @@
 import React from 'react';
 import {
     Box,
-    Icon,
     Text,
     Flex,
+    Stack,
     Heading,
     useMediaQuery
 } from '@chakra-ui/react';
-import {
-    DiAndroid,
-    DiCodeigniter,
-    DiWebplatform
-} from "react-icons/di";
+import Card from "components/Card";
+import { portifolio } from "content.json";
 
 function Portifolio() {
+    const { upperDesc, bottomDesc, projects } = portifolio;
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
-    return <Flex
-        direction={isNotSmallerScreen ? "row" : "column"}
-        w="100%"
-        maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }} >
+    return <Stack>
+        <Flex
+            direction={isNotSmallerScreen ? "row" : "column"}
+            p={isNotSmallerScreen ? "32" : "0"}
+            alignSelf="flex-start" >
 
-        <Box
-            alignSelf="center"
-            p="16" >
+            <Box
+                alignSelf="flex-start"
+                mr={isNotSmallerScreen ? "16" : "0"}
+            >
 
-            <Heading
-                fontWeight="extrabold"
-                color="cyan.500"
-                size="3xl" >
-                Check out
-            </Heading>
-            <Text
-                fontSize="1xl"
-                color="gray.400" >
-                some of my projects
-            </Text>
-        </Box>
+                <Heading
+                    fontWeight="extrabold"
+                    color="cyan.500"
+                    size="3xl" >
+                    {upperDesc}
+                </Heading>
+                <Text
+                    fontSize="1xl"
+                    color="gray.400" >
+                    {bottomDesc}
+                </Text>
+            </Box>
 
-        <Box
-            alignSelf="center"
-            p="16" >
-
-            <Flex
-                direction={isNotSmallerScreen ? "row" : "column"}
-                mt={8} >
-
-                <Flex
-                    rounded="xl"
-                    direction="column"
-                    mt={4}
-                    bg="blue.400"
-                    h="30vh"
-                    w="30vh"
-                    justify="flex-end" >
-
-                    <Icon
-                        color="white"
-                        p="4"
-                        as={DiAndroid}
-                        w="24"
-                        h="24" />
-
-                    <Text
-                        color="white"
-                        p="4"
-                        fontSize="xl"
-                        fontWeight="semibold" >
-                        Android Apps
-                    </Text>
-
-                </Flex>
-
-
+            <Box
+                flexGrow={1}
+                alignSelf="center"
+            >
 
                 <Flex
-                    rounded="xl"
-                    direction="column"
-                    mt={4} ml={isNotSmallerScreen ? 4 : 0}
-                    bg="gray.100"
-                    h="30vh"
-                    w="30vh"
-                    justify="flex-end"
-                    _hover={{ bg: "teal.400", }} >
-
-                    <Icon
-                        color="black"
-                        p="4"
-                        as={DiCodeigniter}
-                        w="24"
-                        h="24" />
-
-                    <Text
-                        color="black"
-                        p="4"
-                        fontSize="xl"
-                        fontWeight="semibold" >
-                        Flutter Apps
-                    </Text>
-
+                    direction={isNotSmallerScreen ? "row" : "column"}
+                    justifyContent="flex-end"
+                    wrap="wrap" >
+                    {projects.map((itemProps, index) => <Card
+                        key={`project${index}`}
+                        {...itemProps} />)}
                 </Flex>
-
-                <Flex
-                    rounded="xl"
-                    direction="column"
-                    mt={4} ml={isNotSmallerScreen ? 4 : 0}
-                    bg="gray.100"
-                    h="30vh"
-                    w="30vh"
-                    justify="flex-end"
-                    _hover={{ bg: "teal.400", }} >
-
-                    <Icon
-                        color="black"
-                        p="4"
-                        as={DiCodeigniter}
-                        w="24"
-                        h="24" />
-
-                    <Text
-                        color="black"
-                        p="4"
-                        fontSize="xl"
-                        fontWeight="semibold" >
-                        Flutter Apps
-                    </Text>
-
-                </Flex>
-
-
-            </Flex>
-        </Box>
-    </Flex>
+            </Box>
+        </Flex>
+    </Stack>
 }
 
 export default Portifolio
