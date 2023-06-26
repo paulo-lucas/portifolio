a14dedc16a5238f18913fdc55ee88544695a14f0
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2023-06-26 11:12:37 -0300

    creating projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Flex,
+  Stack,
+  useMediaQuery
+} from '@chakra-ui/react';
+import Card from "components/Card";
+import { portifolio } from "content.json";
+
+function Projects() {
+  const { projects } = portifolio;
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+  return <Stack>
+    <Flex
+      direction={isNotSmallerScreen ? "row" : "column"}
+      justifyContent="center"
+      wrap="wrap" >
+      {projects.map((itemProps, index) => <Card
+        key={`project${index}`}
+        {...itemProps} />)}
+    </Flex>
+  </Stack>
+}
+
+export default Projects
