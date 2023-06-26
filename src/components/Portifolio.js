1d147e28806a03cb3b64e826d99915f6d2113430
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2023-06-26 11:12:54 -0300

    creating portifolio tags and adjusting content

--- src/components/Portifolio.js
+++ src/components/Portifolio.js
@@ -1,57 +1,58 @@
 import React from 'react';
 import {
-    Box,
-    Text,
-    Flex,
+    Tabs,
+    Tab,
+    TabPanels,
+    TabPanel,
+    TabList,
     Stack,
+    Box,
     Heading,
+    Text,
     useMediaQuery
 } from '@chakra-ui/react';
-import Card from "components/Card";
-import { portifolio } from "content.json";
+import { portifolio } from 'content.json'
+import Projects from "components/Projects";
 
 function Portifolio() {
-    const { upperDesc, bottomDesc, projects } = portifolio;
+    const { upperDesc, bottomDesc } = portifolio
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
-    return <Stack>
-        <Flex
-            direction={isNotSmallerScreen ? "row" : "column"}
-            p={isNotSmallerScreen ? "32" : "0"}
-            alignSelf="flex-start" >
+    return <Stack px={isNotSmallerScreen ? "32" : "4"} >
+        <Box
+            alignSelf="flex-start"
+            mb={8} >
 
-            <Box
-                alignSelf="flex-start"
-                mr={isNotSmallerScreen ? "16" : "0"} >
+            <Heading
+                fontWeight="extrabold"
+                color="cyan.500"
+                size="3xl" >
+                {upperDesc}
+            </Heading>
+            <Text
+                fontSize="1xl"
+                color="gray.400" >
+                {bottomDesc}
+            </Text>
+        </Box>
 
-                <Heading
-                    fontWeight="extrabold"
-                    color="cyan.500"
-                    size="3xl" >
-                    {upperDesc}
-                </Heading>
-                <Text
-                    fontSize="1xl"
-                    color="gray.400" >
-                    {bottomDesc}
-                </Text>
-            </Box>
-
-            <Box
-                flexGrow={1}
-                alignSelf="center"
-            >
-
-                <Flex
-                    direction={isNotSmallerScreen ? "row" : "column"}
-                    justifyContent="flex-end"
-                    wrap="wrap" >
-                    {projects.map((itemProps, index) => <Card
-                        key={`project${index}`}
-                        {...itemProps} />)}
-                </Flex>
-            </Box>
-        </Flex>
+        <Tabs
+            isFitted
+            variant='line'
+            w="100%">
+            <TabList mb='1em'>
+                <Tab>Projects</Tab>
+                <Tab>Designs</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Projects />
+                </TabPanel>
+                <TabPanel>
+                    <p>two!</p>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     </Stack>
 }
 
